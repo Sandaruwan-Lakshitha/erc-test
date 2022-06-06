22b06506a914739722272b62b4cdc66ef0250b91
Sandaruwan Lakshitha <sandaru.laka@gmail.com>
2022-06-07 00:22:12 +0530

    init

---
+++ src/main/java/com/g7/ercsystem/ErcsystemApplication.java
@@ -0,0 +1,13 @@
+package com.g7.ercsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErcsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErcsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/g7/ercsystem/ErcsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g7.ercsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErcsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
